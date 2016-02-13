a199c9f9f0e849bbbf4c7020667f7d5c038beaf6
Heiko Hirschmueller <heiko.hirschmueller@roboception.de>
2016-02-13 20:46:17 +0100

    Reimplementation of downscaleImage() to make it much faster

--- gimage/size.h
+++ gimage/size.h
@@ -38,6 +38,8 @@
 
 #include "image.h"
 
+#include <gutil/proctime.h>
+
 #include <vector>
 
 namespace gimage
@@ -47,29 +49,154 @@ template<class T> Image<T> downscaleImage(const Image<T> &image, int factor)
 {
     factor=std::max(1, factor);
     
+    if (factor == 1)
+      return image;
+    
     Image<T> ret((image.getWidth()+factor-1)/factor,
       (image.getHeight()+factor-1)/factor, image.getDepth());
     
     for (int d=0; d<image.getDepth(); d++)
     {
-      for (long k=0; k<image.getHeight(); k+=factor)
+      T *out=ret.getPtr(0, 0, d);
+      
+      long k=0;
+      while (k+factor <= image.getHeight())
+      {
+        long i=0;
+        
+        if (factor == 2)
+        {
+          // speeding up special case of averaging over 2*2 pixels
+          
+          while (i+2 <= image.getWidth())
+          {
+            typename Image<T>::work_t v;
+            
+            const T *in=image.getPtr(i, k, d);
+            
+            v=*in+in[1];
+            in+=image.getWidth();
+            v+=*in+in[1];
+            
+            *out++=static_cast<typename Image<T>::store_t>((v+2)/4);
+            i+=2;
+          }
+        }
+        else if (factor == 3)
+        {
+          // speeding up special case of averaging over 3*3 pixels
+          
+          while (i+3 <= image.getWidth())
+          {
+            typename Image<T>::work_t v=0;
+            
+            const T *in=image.getPtr(i, k, d);
+            
+            for (int j=0; j<3; j++)
+            {
+              v+=*in+in[1]+in[2];
+              in+=image.getWidth();
+            }
+            
+            *out++=static_cast<typename Image<T>::store_t>((v+4)/9);
+            i+=3;
+          }
+        }
+        else if (factor == 4)
+        {
+          // speeding up special case of averaging over 4*4 pixels
+          
+          while (i+4 <= image.getWidth())
+          {
+            typename Image<T>::work_t v=0;
+            
+            const T *in=image.getPtr(i, k, d);
+            
+            for (int j=0; j<4; j++)
+            {
+              v+=*in+in[1]+in[2]+in[3];
+              in+=image.getWidth();
+            }
+            
+            *out++=static_cast<typename Image<T>::store_t>((v+8)/16);
+            i+=4;
+          }
+        }
+        else
+        {
+          // average over factor*factor pixels of the input image
+          
+          while (i+factor <= image.getWidth())
+          {
+            typename Image<T>::work_t v=0;
+            int n=0;
+            
+            const T *in=image.getPtr(i, k, d);
+            for (int kk=0; kk<factor; kk++)
+            {
+              for (int ii=0; ii<factor; ii++)
+              {
+                v+=in[ii];
+                n++;
+              }
+              
+              in+=image.getWidth();
+            }
+            
+            *out++=static_cast<typename Image<T>::store_t>((v+(n>>1))/n);
+            i+=factor;
+          }
+        }
+        
+        // if there are less than factor pixels left in the image row, then
+        // average with boundary check
+        
+        if (i < image.getWidth())
+        {
+          typename Image<T>::work_t v=0;
+          int n=0;
+          
+          const T *in=image.getPtr(i, k, d);
+          for (int kk=0; kk<factor; kk++)
+          {
+            for (int ii=0; ii<factor && i+ii<image.getWidth(); ii++)
+            {
+              v+=in[ii];
+              n++;
+            }
+            
+            in+=image.getWidth();
+          }
+          
+          *out++=static_cast<typename Image<T>::store_t>((v+(n>>1))/n);
+        }
+        
+        k+=factor;
+      }
+      
+      // if there are less than factor image rows left in the image, then
+      // average with boundary check
+      
+      if (k < image.getHeight())
       {
         for (long i=0; i<image.getWidth(); i+=factor)
         {
           typename Image<T>::work_t v=0;
           int n=0;
           
+          const T *in=image.getPtr(i, k, d);
           for (int kk=0; kk<factor && k+kk<image.getHeight(); kk++)
           {
             for (int ii=0; ii<factor && i+ii<image.getWidth(); ii++)
             {
-              v+=image.get(i+ii, k+kk, d);
+              v+=in[ii];
               n++;
             }
+            
+            in+=image.getWidth();
           }
           
-          if (n > 0)
-            ret.set(i/factor, k/factor, d, static_cast<typename Image<T>::store_t>(v/n));
+          *out++=static_cast<typename Image<T>::store_t>((v+(n>>1))/n);
         }
       }
     }
